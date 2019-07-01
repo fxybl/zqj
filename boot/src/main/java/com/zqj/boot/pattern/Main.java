63f5e473dd79d99f7d1f9f0bc05b25d000a8ba9f
zqj <502527463@qq.com>
2019-07-01 19:08:03 +0800

    主函数

---
+++ boot/src/main/java/com/zqj/boot/pattern/Main.java
@@ -0,0 +1,17 @@
+package com.zqj.boot.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zqj
+ * @program com.zqj.boot.pattern
+ * @description Main
+ * @create 2019-07-01 18:59
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
