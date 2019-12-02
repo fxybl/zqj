f5c8d9c39b0c06a880fa15f7734b81d7d694f556
zhaoquanjie <502527463@qq.com>
2019-12-02 10:11:23 +0800

    自写FutureTask

--- boot/src/main/java/com/zqj/jdk/future/MyFutureTask.java
+++ boot/src/main/java/com/zqj/jdk/future/MyFutureTask.java
@@ -78,8 +78,8 @@ public class MyFutureTask<V> implements Runnable {
 
     }
 
-    public MyFutureTask(MyCallable callable){
-        if(callable==null){
+    public MyFutureTask(MyCallable callable) {
+        if (callable == null) {
             throw new NullPointerException();
         }
         this.callable = callable;
@@ -88,121 +88,121 @@ public class MyFutureTask<V> implements Runnable {
 
     @Override
     public void run() {
-        if(state !=NEW || !UNSAFE.compareAndSwapObject(this,runnerOffset,null,Thread.currentThread())){
+        if (state != NEW || !UNSAFE.compareAndSwapObject(this, runnerOffset, null, Thread.currentThread())) {
             //如果此时状态不是NEW,或者改变当前持有线程的时候失败，则直接返回。这样保证只有一个线程在执行此方法（因为会进行2个非原子性操作，1个new对象时改变state为NEW，一个run时改变持有的线程。）
             return;
         }
-        try{
+        try {
             //用另一个指针指向callable对象
             MyCallable<V> callable = this.callable;
 
-            if(callable !=null && state ==NEW){
+            if (callable != null && state == NEW) {
                 //运行结果
                 boolean ran;
                 V result;
-                try{
+                try {
                     result = callable.call();
-                    ran= true;
+                    ran = true;
 
-                }catch (Exception e){
+                } catch (Throwable e) {
                     result = null;
                     ran = false;
                     setException(e);
                 }
-                if(ran){
+                if (ran) {
                     set(result);
                 }
             }
 
-        }finally {
+        } finally {
             //线程执行完则取消当前线程的绑定
             runner = null;
         }
     }
 
     //获取结果
-    public V get() throws Exception{
+    public V get() throws Exception {
         int s = state;
         //任务在进行中，则睡眠线程,等待被唤醒
-        if(state <= COMPLETING){
+        if (state <= COMPLETING) {
             //不超时返回，
-            s=awaitDone(false,0L);
+            s = awaitDone(false, 0L);
         }
         return report(s);
     }
 
     //获取结果，超时，任然没数据，则抛出超时异常
-    public V get(long timeout, TimeUnit unit) throws TimeoutException,InterruptedException, ExecutionException {
-        if(unit ==null){
+    public V get(long timeout, TimeUnit unit) throws TimeoutException, InterruptedException, ExecutionException {
+        if (unit == null) {
             throw new NullPointerException();
         }
         int s = state;
         //如果awaitDone()执行了后，超时返回的结果还是小于等于COMPLETING,则抛出超时异常。
-        if(s  <= COMPLETING && (s =awaitDone(true,unit.toNanos(timeout))) <=COMPLETING){
+        if (s <= COMPLETING && (s = awaitDone(true, unit.toNanos(timeout))) <= COMPLETING) {
             throw new TimeoutException();
         }
-        return  report(s);
+        return report(s);
     }
 
-    private V report(int s) throws ExecutionException{
+    private V report(int s) throws ExecutionException {
         Object object = outcome;
         //正常状态直接返回
-        if(s ==NORMAL){
-            return (V)object;
+        if (s == NORMAL) {
+            return (V) object;
         }
         //放弃之后的都为手动取消
-        if(s >= CANCLED){
+        if (s >= CANCLED) {
             throw new CancellationException();
         }
         //剩下一个异常EXCEPTIONAL状态
-        return null;
+        throw new ExecutionException((Throwable) object);
     }
 
     private int awaitDone(boolean timed, long nanos) throws InterruptedException {
         //超时的时间，如果设置超时返回,timed为true,则计算当前时间加上超时的时候之后的时间点。否则为0;
-        final long deadline = timed ? System.nanoTime()+nanos : 0L;
+        final long deadline = timed ? System.nanoTime() + nanos : 0L;
         //定义一个node
-        WaiterNode node =null;
+        WaiterNode node = null;
         boolean queued = false;
         //自旋
-        for( ; ;){
-           //首先响应中断
+        for (; ; ) {
+            //首先响应中断
             //此方法会判断此线程是否被中断，并且会清除中断状态
-            if(Thread.interrupted()){
+            if (Thread.interrupted()) {
                 //被中断移除此节点，node为空不会被处理
                 removeWaiter(node);
                 throw new InterruptedException();
             }
             int s = state;
             //此时状态大于COMPLETING则表示已经处理完成,可以直接返回状态
-            if(s >COMPLETING){
+            if (s > COMPLETING) {
                 return s;
-            }else if(s ==COMPLETING){
+            } else if (s == COMPLETING) {
                 //此时刚好是COMPLETING,则表示任务可能即将被完成，设置线程的优先级为低，释放执行权，让其他线程先执行
                 Thread.yield();
             }
             //剩下的就是状态是NEW的任务了
             //初次进来node是为空的
-            else if(node ==null){
+            else if (node == null) {
                 node = new WaiterNode();
-            }else if(!queued){
+            } else if (!queued) {
                 //初次!queued必为true，此时通过cas将waters改为自己定义的新的WaiterNode node(也就是将新的等待队列置于waiters链表的头部),同时将node的下一个节点指向之前的老的waiters，如果更新失败，下次进来继续更新
                 //更新前 oldHeadWaiterNode -->WaiterNode -->oldTrail
                 //更新后  newHeadWaiterNode -->oldHeadWaiterNode -->WaiterNode -->oldTrail
-                queued = UNSAFE.compareAndSwapObject(this,waitersOffset,node.next = waiters,node);
-            }else if(timed){
+                queued = UNSAFE.compareAndSwapObject(this, waitersOffset, node.next = waiters, node);
+            } else if (timed) {
                 //设置了超时返回
                 //用设置的时间期限减去当前时间，如果小于等于0则表示已经超时，应该返回值了，否则睡眠线程
-                 nanos = deadline -System.nanoTime();
-                 if(nanos<=0L){
-                     //在等待waiters中移除当前节点
-                     removeWaiter(node);
-                     return state;
-                 }else {
-                     //睡眠线程
-                     LockSupport.park(this);
-                 }
-            }else {
+                nanos = deadline - System.nanoTime();
+                if (nanos <= 0L) {
+                    //在等待waiters中移除当前节点
+                    removeWaiter(node);
+                    return state;
+                } else {
+                    //睡眠线程
+                    LockSupport.park(this);
+                }
+            } else {
                 //未设置超时返回,直接睡眠线程
                 LockSupport.park(this);
             }
@@ -211,7 +211,7 @@ public class MyFutureTask<V> implements Runnable {
     }
 
     private void removeWaiter(WaiterNode node) {
-        if(node !=null){
+        if (node != null) {
 
         }
     }
@@ -219,11 +219,11 @@ public class MyFutureTask<V> implements Runnable {
     //执行成功设置值
     private void set(V result) {
         //this替换的对应的对象，stateOffset内存地址值,NEW原先值,COMPLETING期待替换后的值
-        if(UNSAFE.compareAndSwapInt(this,stateOffset,NEW,COMPLETING)){
+        if (UNSAFE.compareAndSwapInt(this, stateOffset, NEW, COMPLETING)) {
             //改变成功则设置结果
             outcome = result;
             //强制改变状态为正常完成
-            UNSAFE.putOrderedInt(this,stateOffset,NORMAL);
+            UNSAFE.putOrderedInt(this, stateOffset, NORMAL);
             //唤醒
             finishCompletion();
         }
@@ -231,13 +231,13 @@ public class MyFutureTask<V> implements Runnable {
     }
 
     //callable执行失败设置异常
-    private void setException(Exception e) {
+    private void setException(Throwable e) {
         //尝试改变state为completing
-        if(UNSAFE.compareAndSwapInt(this,stateOffset,NEW,COMPLETING)){
+        if (UNSAFE.compareAndSwapInt(this, stateOffset, NEW, COMPLETING)) {
             //改变成功后，设置结果
             outcome = e;
             //设置后，强制改变state为异常的状态
-            UNSAFE.putOrderedInt(this,stateOffset,EXCEPTIONAL);
+            UNSAFE.putOrderedInt(this, stateOffset, EXCEPTIONAL);
             //改变后唤醒通知get()时所有休眠的线程，通知他们已经有结果了
             finishCompletion();
         }
@@ -247,21 +247,21 @@ public class MyFutureTask<V> implements Runnable {
     private void finishCompletion() {
         //遍历waiters这个链状结构的等待队列,依次唤醒，并清除掉。
         //w指向waters对象，然后将waters置为空，如果失败，则继续循环，直到成功改变为空，停止循环。
-        for(WaiterNode w;(w=waiters)!=null ;){
+        for (WaiterNode w; (w = waiters) != null; ) {
             //使用cas将waters队列引用置空。
-            if(UNSAFE.compareAndSwapObject(this,waitersOffset,w,null)){
-                for( ; ;){
+            if (UNSAFE.compareAndSwapObject(this, waitersOffset, w, null)) {
+                for (; ; ) {
                     //用t指向thread对象
                     Thread t = w.thread;
-                    if(t!=null){
+                    if (t != null) {
                         //help gc
                         w.thread = null;
                         //唤醒此线程
                         LockSupport.unpark(t);
                     }
                     //获取此节点的下一个等待中的节点
-                    WaiterNode n =w.next;
-                    if(n==null){
+                    WaiterNode n = w.next;
+                    if (n == null) {
                         //不存在下一个则停止循环
                         break;
                     }
@@ -272,7 +272,7 @@ public class MyFutureTask<V> implements Runnable {
                     //它是某个长生存期对象的成员，并且再也不会被该对象使用，并且已经被 dispose 了以释放其占用的资源。这里将其设为 null 是一种安全的做法，因为确定某人误用了一个 null 对象比确定某人误用了一个已被 dispose 的对象更容易些。
                     //help gc,断开w和next的连接，然后让w重新指向next,可以加速w之前指向的对象的垃圾回收速度。
                     w.next = null;
-                    w=n;
+                    w = n;
                 }
                 //全部清理完成后停止最外面的循环
                 break;
@@ -291,7 +291,7 @@ public class MyFutureTask<V> implements Runnable {
 
     public static void main(String[] args) {
         //无限循环
-        for(int i;(i=3)>0;){
+        for (int i; (i = 3) > 0; ) {
             i--;
             System.out.println(i);
         }
