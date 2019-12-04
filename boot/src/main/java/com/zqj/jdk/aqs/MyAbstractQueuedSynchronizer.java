8f2645e439744482996c3503acd831d06316cda8
zhaoquanjie <502527463@qq.com>
2019-12-04 17:51:27 +0800

    自写AQS

--- boot/src/main/java/com/zqj/jdk/aqs/MyAbstractQueuedSynchronizer.java
+++ boot/src/main/java/com/zqj/jdk/aqs/MyAbstractQueuedSynchronizer.java
@@ -1,8 +1,24 @@
 package com.zqj.jdk.aqs;
 
+import java.io.Serializable;
+
 /**
  * @author zqj
  * @create 2019-12-04 11:02
  */
-public abstract class MyAbstractQueuedSynchronizer {
+public abstract class MyAbstractQueuedSynchronizer implements Serializable {
+
+    private static final long serialVersionUID = 7373984972572414692L;
+
+    /*
+     *链表结构
+    *   * <pre>
+     *      +------+  prev +-----+       +-----+
+     * head |      | <---- |     | <---- |     |  tail
+     *      +------+       +-----+       +-----+
+     * </pre>
+    *
+    * */
+
+
 }
