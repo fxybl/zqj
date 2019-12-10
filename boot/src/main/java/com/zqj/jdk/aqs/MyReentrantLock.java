dfd732a9211dc017569310088a431b553e2cb614
zhaoquanjie <502527463@qq.com>
2019-12-10 11:04:56 +0800

    自写aqs

--- boot/src/main/java/com/zqj/jdk/aqs/MyReentrantLock.java
+++ boot/src/main/java/com/zqj/jdk/aqs/MyReentrantLock.java
@@ -10,7 +10,36 @@ public class MyReentrantLock implements Serializable {
 
     private static final long serialVersionUID = 7373984972572414698L;
 
-    public void lock(){
+    abstract static class Sync extends MyAbstractQueuedSynchronizer{
 
+        private static final long serialVersionUID = 7373984972572414688L;
+
+       abstract void lock();
+
+        @Override
+        protected boolean tryRelease(int args) {
+            return false;
+        }
+
+        @Override
+        protected boolean tryAcquire(int args) {
+            return false;
+        }
+    }
+
+    //非公平同步锁
+    static final class NonfairSync extends Sync{
+        @Override
+        void lock() {
+
+        }
+    }
+
+    //公平同步锁
+    static final class FairSync extends Sync{
+        @Override
+        void lock() {
+
+        }
     }
 }
